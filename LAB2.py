afb5153df0c5e13ea3acb072105bdb2ab748e5a6
cairqq <c4rinabui11@gmail.com>
2025-06-17 10:55:34 -0700

    completed lab2

---
+++ LAB2.py
@@ -0,0 +1,20 @@
+# lab 2
+
+## step 1
+name = 'carina bui'
+
+## step 2
+nickname = 'cari'
+
+## step 3 
+age = 15
+
+## step 4
+used_python = False
+
+## step 5
+hobbies =["video games", "music", "eating"]
+
+## step 6
+favorite_things = {"game": "valorant", "food": "hotpot", "color": "red"}
+
